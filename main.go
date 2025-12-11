76655e3f1568b4f19bbc18c37bdd4e0ce55a6c4a
Keita86710 <208435708+Keita86710@users.noreply.github.com>
2025-12-11 16:40:33 +0900

    test push

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello from Go API!")
+    http.HandleFunc("/api/kon", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "konnitiwa!")
     })
-
+    
     fmt.Println("Backend running on :8080")
     http.ListenAndServe(":8080", nil)
 }
