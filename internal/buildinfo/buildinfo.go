68937f1a7a9726c33e33a8ad63f410117631efbe
shizuku86 <shizuku86710@gmail.com>
2025-12-15 20:13:06 +0900

    create local dev env (#13)

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -1,8 +1,26 @@
 package buildinfo
 
+import "os"
+
 var (
-    AppEnv    = "unknown"
-    Version   = "unknown"
-    BuildTime = "unknown"
-    GitCommit = "unknown"
+	AppEnvLd    = ""
+	VersionLd   = ""
+	BuildTimeLd = ""
+	GitCommitLd = ""
 )
+
+var (
+	AppEnv    = value(AppEnvLd, os.Getenv("APP_ENV"), "unknown")
+	Version   = value(VersionLd, os.Getenv("VERSION"), "unknown")
+	BuildTime = value(BuildTimeLd, os.Getenv("BUILD_TIME"), "unknown")
+	GitCommit = value(GitCommitLd, os.Getenv("GIT_COMMIT"), "unknown")
+)
+
+func value(values ...string) string {
+	for _, v := range values {
+		if v != "" && v != "unknown" {
+			return v
+		}
+	}
+	return "unknown"
+}
